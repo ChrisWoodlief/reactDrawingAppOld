00354cbcd47703dfc67255fb183ea5424533c451
Chris Woodlief <chriswoodlief@chriss-m1-mbp.lan>
2022-04-18 10:36:16 -0400

    add in react router and some initial pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to React Router!</h1>
+        <Routes>
+          <Route path="/" element={<DrawingsFeed />} />
+          <Route path="login" element={<Login />} />
+          <Route path="create" element={<Create />} />
+        </Routes>
       </header>
     </div>
   );
 }
+
+function DrawingsFeed() {
+  return (
+    <>
+      Drawings Feed here
+    </>
+  );
+}
+
+function Login() {
+  return (
+    <>
+      Login page here
+    </>
+  );
+}
+
+function Create(){
+  return (
+    <>
+      Create page here
+    </>
+  )
+}
+
 
 export default App;
